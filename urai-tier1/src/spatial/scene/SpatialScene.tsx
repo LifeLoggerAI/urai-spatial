37dc8017e94d7e4982e57040358497dbea30b7fa
Adam Clamp <yourgithubemail@example.com>
2026-03-17 03:33:14 +0000

    feat(urai-spatial): phase60 story bundle review deck

--- urai-tier1/src/spatial/scene/SpatialScene.tsx
+++ urai-tier1/src/spatial/scene/SpatialScene.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SpatialStoryReviewDeckPanel from "@/spatial/ui/SpatialStoryReviewDeckPanel";
 import SpatialBundleLineagePanel from "@/spatial/ui/SpatialBundleLineagePanel";
 import SpatialStoryBundleDiffPanel from "@/spatial/ui/SpatialStoryBundleDiffPanel";
 import SpatialStoryBundleVaultPanel from "@/spatial/ui/SpatialStoryBundleVaultPanel";
@@ -766,6 +767,7 @@ export default function SpatialScene() {
       <SpatialStoryBundleVaultPanel />
       <SpatialStoryBundleDiffPanel />
       <SpatialBundleLineagePanel />
+      <SpatialStoryReviewDeckPanel />
       <Canvas
         camera={{ position: [0, 6, 30], fov: 42, near: 0.1, far: 200 }}
         dpr={[1, 2]}
