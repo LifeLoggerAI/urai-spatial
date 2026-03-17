df6f1e4b43af84fa4f0a6a2dbcb9c07b1dd94404
Adam Clamp <yourgithubemail@example.com>
2026-03-17 04:17:31 +0000

    feat(urai-spatial): phase74 curated deck divergence panel

--- urai-tier1/src/spatial/scene/SpatialScene.tsx
+++ urai-tier1/src/spatial/scene/SpatialScene.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SpatialCuratedDeckDivergencePanel from "@/spatial/ui/SpatialCuratedDeckDivergencePanel";
 import SpatialCuratedDeckConsensusPanel from "@/spatial/ui/SpatialCuratedDeckConsensusPanel";
 import SpatialCuratedDeckAnchorPanel from "@/spatial/ui/SpatialCuratedDeckAnchorPanel";
 import SpatialCuratedDeckDriftPanel from "@/spatial/ui/SpatialCuratedDeckDriftPanel";
@@ -794,6 +795,7 @@ export default function SpatialScene() {
       <SpatialCuratedDeckDriftPanel />
       <SpatialCuratedDeckAnchorPanel />
       <SpatialCuratedDeckConsensusPanel />
+      <SpatialCuratedDeckDivergencePanel />
       <Canvas
         camera={{ position: [0, 6, 30], fov: 42, near: 0.1, far: 200 }}
         dpr={[1, 2]}
