ef339dd1ef12586964cd99f92124317e4098a51b
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-03 18:46:45 -0500

    Replace snapshot imports with canonical spatial modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import SpatialScene from '../../*audit/20260402_175733*PASS_T1_RECOVERY_LOCK/backup/src/spatial/scene/SpatialScene'
-import { CanonicalTierLockHud } from '../../urai-tier1/*audit/20260419_021801*tier2_camera_motion_lock/src/spatial/components/CinematicCameraRig'
+import SpatialScene from '../spatial/scene/SpatialScene'
+import { CanonicalTierLockHud } from '../spatial/components/CanonicalTierLockHud'
 
 export default function HomePage() {
   return (
