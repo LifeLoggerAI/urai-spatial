b2d791132c627750933891075f83f5cd877a46d1
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-04 13:20:27 -0500

    Polish home world layers for sky horizon ground and orb

--- src/app/focus/page.tsx
+++ src/app/focus/page.tsx
@@ -1,7 +1,7 @@
 import SpatialScene from '@/spatial/scene/SpatialScene'
 import LifeMapCanonicalSurface from '@/spatial/components/LifeMapCanonicalSurface'
 
-export default function ReplayPage() {
+export default function FocusPage() {
   return (
     <>
       <SpatialScene />
--- src/app/mirror/page.tsx
+++ src/app/mirror/page.tsx
@@ -1,7 +1,7 @@
 import SpatialScene from '@/spatial/scene/SpatialScene'
 import LifeMapCanonicalSurface from '@/spatial/components/LifeMapCanonicalSurface'
 
-export default function ReplayPage() {
+export default function MirrorPage() {
   return (
     <>
       <SpatialScene />
