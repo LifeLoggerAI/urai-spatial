3e2c9e564e85a292d54132fcbcd414af51c08b33
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-04 03:25:42 -0500

    Add Home World avatar layer

---
+++ urai-tier1/src/spatial/home/layers/AvatarLayer.tsx
@@ -0,0 +1,14 @@
+export function AvatarLayer() {
+  return (
+    <div className="avatar" data-testid="urai-home-avatar">
+      <div className="avatar-aura" />
+      <div className="avatar-head" />
+      <div className="avatar-torso" />
+      <div className="avatar-arm avatar-arm-left" />
+      <div className="avatar-arm avatar-arm-right" />
+      <div className="avatar-leg avatar-leg-left" />
+      <div className="avatar-leg avatar-leg-right" />
+      <div className="avatar-shadow" />
+    </div>
+  );
+}
