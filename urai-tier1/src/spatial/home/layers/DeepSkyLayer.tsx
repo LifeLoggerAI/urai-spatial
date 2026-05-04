b416010b88519585f499a3fa97679f14a365f500
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-04 03:05:01 -0500

    Add Home World sky layer components

---
+++ urai-tier1/src/spatial/home/layers/DeepSkyLayer.tsx
@@ -0,0 +1,15 @@
+export function DeepSkyLayer() {
+  return <div className="deep-sky" data-testid="home-layer-deep-sky" />;
+}
+
+export function CelestialPortalLayer() {
+  return <div className="sky-portal" data-testid="home-layer-sky-portal" />;
+}
+
+export function AuroraWeatherLayer() {
+  return <div className="aurora" data-testid="home-layer-aurora" />;
+}
+
+export function CloudLayer() {
+  return <><div className="cloud cloud-a" /><div className="cloud cloud-b" /></>;
+}
