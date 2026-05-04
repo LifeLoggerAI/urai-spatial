19cca5149b1c6b0c54329dded7cf289e3bc07013
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-04 05:12:39 -0500

    Use canonical LifeMap surface as app entry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import SpatialScene from '@/spatial/scene/SpatialScene'
 import { CanonicalTierLockHud } from '@/spatial/components/CanonicalTierLockHud'
-import LifeMapCompletionOverlay from '@/spatial/components/LifeMapCompletionOverlay'
+import LifeMapCanonicalSurface from '@/spatial/components/LifeMapCanonicalSurface'
 
 const publicDemoMode = process.env.NEXT_PUBLIC_PUBLIC_DEMO_MODE === 'true'
 const recordingMode = process.env.NEXT_PUBLIC_RECORDING_MODE === 'true'
@@ -9,8 +8,7 @@ const showInternalHud = !publicDemoMode && !recordingMode
 export default function HomePage() {
   return (
     <>
-      <SpatialScene />
-      <LifeMapCompletionOverlay />
+      <LifeMapCanonicalSurface />
       {showInternalHud ? <CanonicalTierLockHud /> : null}
     </>
   )
