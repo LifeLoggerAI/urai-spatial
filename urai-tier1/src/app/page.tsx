601bd97cee69b87f649ab534a8ed30736851a4a0
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-05 20:55:00 -0500

    Wrap home scene in suspense boundary

--- urai-tier1/src/app/page.tsx
+++ urai-tier1/src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import HomeScene from "@/scene/HomeScene";
 import { SpatialShell } from "@/spatial/layout/SpatialShell";
 
 const HomePage: React.FC = () => {
   return (
     <SpatialShell mode="overview" sourceBadge="demo">
-      <HomeScene />
+      <Suspense fallback={<div style={{ color: "white", padding: 24 }}>Loading URAI Spatial...</div>}>
+        <HomeScene />
+      </Suspense>
     </SpatialShell>
   );
 };
