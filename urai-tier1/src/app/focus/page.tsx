45d4a2ec92abbc809e5950240e4b827f6ab117c0
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-05 21:45:50 -0500

    Polish focus route fallback

--- urai-tier1/src/app/focus/page.tsx
+++ urai-tier1/src/app/focus/page.tsx
@@ -1,14 +1,5 @@
-import { Suspense } from "react";
-import FocusRouteStage from "@/spatial/scene/FocusRouteStage";
-import { SpatialShell } from "@/spatial/layout/SpatialShell";
-import { SpatialLoadingState } from "@/spatial/components/states/SpatialStates";
+import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
 
 export default function FocusRoute() {
-  return (
-    <SpatialShell mode="detail" sourceBadge="firestore" timeline={<span>Memory Detail</span>}>
-      <Suspense fallback={<SpatialLoadingState />}>
-        <FocusRouteStage />
-      </Suspense>
-    </SpatialShell>
-  );
+  return <TierOneExperience mode="focus" />;
 }
