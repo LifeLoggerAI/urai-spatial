b02716e22b6d8c5ad45f84fa4489e10617ce5df6
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-05 21:25:38 -0500

    Route life-map to Tier-1 constellation shell

--- urai-tier1/src/app/life-map/LifeMapClient.tsx
+++ urai-tier1/src/app/life-map/LifeMapClient.tsx
@@ -1,27 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-
-const SpatialSceneClient = dynamic(() => import("../../spatial/client/SpatialSceneClient"), {
-  ssr: false,
-});
+import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
 
 export default function LifeMapClient() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <main>
-        <h1>Life Map</h1>
-        <p>Loading spatial scene...</p>
-      </main>
-    );
-  }
-
-  return <SpatialSceneClient />;
+  return <TierOneExperience mode="life-map" />;
 }
