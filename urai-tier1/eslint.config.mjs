f29fccc78bf992a301625df1bb50c7dd2d560a87
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-07 01:49:31 -0500

    Harden ESLint flat config imports

--- urai-tier1/eslint.config.mjs
+++ urai-tier1/eslint.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
+function configArray(config) {
+  return Array.isArray(config) ? config : [config];
+}
+
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...configArray(nextVitals),
+  ...configArray(nextTs),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
