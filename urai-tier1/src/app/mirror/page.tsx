33e50e953a54e8f5414fb0a4146227dc6a588695
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-07 14:19:36 -0500

    Wire mirror return affordance

--- urai-tier1/src/app/mirror/page.tsx
+++ urai-tier1/src/app/mirror/page.tsx
@@ -1,5 +1,6 @@
 import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
+import { MirrorReturnControls } from "./MirrorReturnControls";
 
 export default function MirrorRoute() {
-  return <TierOneExperience mode="mirror" />;
+  return <TierOneExperience mode="mirror" cta={<MirrorReturnControls />} />;
 }
