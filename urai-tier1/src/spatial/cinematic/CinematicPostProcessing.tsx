7091f927e17a527a5016a6e12dccf0194af49f7a
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-06 20:33:50 -0500

    feat: add reduced motion postprocessing preset

--- urai-tier1/src/spatial/cinematic/CinematicPostProcessing.tsx
+++ urai-tier1/src/spatial/cinematic/CinematicPostProcessing.tsx
@@ -4,12 +4,19 @@ import { EffectComposer, Bloom, Vignette, ChromaticAberration } from '@react-thr
 import { BlendFunction } from 'postprocessing'
 import { Vector2 } from 'three'
 
-export default function CinematicPostProcessing({ active }: { active: boolean }) {
+export default function CinematicPostProcessing({ active, reducedMotion = false }: { active: boolean; reducedMotion?: boolean }) {
+  if (!active) return null
+
   return (
     <EffectComposer multisampling={0} enabled={active}>
-      <Bloom intensity={0.82} luminanceThreshold={0.16} luminanceSmoothing={0.62} mipmapBlur />
-      <ChromaticAberration blendFunction={BlendFunction.NORMAL} offset={new Vector2(0.0008, 0.0006)} />
-      <Vignette eskil={false} offset={0.18} darkness={0.72} />
+      <Bloom
+        intensity={reducedMotion ? 0.38 : 0.82}
+        luminanceThreshold={reducedMotion ? 0.24 : 0.16}
+        luminanceSmoothing={0.62}
+        mipmapBlur={!reducedMotion}
+      />
+      {!reducedMotion ? <ChromaticAberration blendFunction={BlendFunction.NORMAL} offset={new Vector2(0.0008, 0.0006)} /> : null}
+      <Vignette eskil={false} offset={0.18} darkness={reducedMotion ? 0.5 : 0.72} />
     </EffectComposer>
   )
 }
