4a85bc231a4f990fc0f9174ea3234b81fac2caaa
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-07 14:49:13 -0500

    fix: remove broken next eslint imports on main

--- urai-tier1/eslint.config.mjs
+++ urai-tier1/eslint.config.mjs
@@ -1,21 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-function configArray(config) {
-  return Array.isArray(config) ? config : [config];
-}
 
 const eslintConfig = defineConfig([
-  ...configArray(nextVitals),
-  ...configArray(nextTs),
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
   ]),
 ]);
 
