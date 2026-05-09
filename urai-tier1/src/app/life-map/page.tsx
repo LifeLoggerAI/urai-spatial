4211abffd1213ebbeaabeede62f6052148cc2396
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 10:22:50 -0500

    Route Life Map to trust loop shell

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,5 +1,5 @@
-import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
+import LifeMapTrustLoop from "@/spatial/lifemap/LifeMapTrustLoop";
 
 export default function LifeMapPage() {
-  return <TierOneExperience mode="life-map" />;
+  return <LifeMapTrustLoop />;
 }
