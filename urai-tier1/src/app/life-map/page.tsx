73f032572843791002fa532df6e61c7916f97005
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 09:21:43 -0500

    Route Life Map through Ascent gate

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,9 +1,5 @@
-import SpatialWorldCanvas from "@/spatial/components/world/SpatialWorldCanvas";
+import LifeMapAscentGate from "@/spatial/components/world/LifeMapAscentGate";
 
 export default function LifeMapPage() {
-  return (
-    <div data-testid="lifemap-starfield" data-urai-spatial-stage="life-map">
-      <SpatialWorldCanvas mode="life-map" />
-    </div>
-  );
+  return <LifeMapAscentGate />;
 }
