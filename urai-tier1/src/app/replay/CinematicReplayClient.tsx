c6cddf4ee9cdc84b8e7905e72fc1a7be7cb72134
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 10:22:09 -0500

    Pass replay scrubbing state through timeline

--- urai-tier1/src/app/replay/CinematicReplayClient.tsx
+++ urai-tier1/src/app/replay/CinematicReplayClient.tsx
@@ -190,6 +190,7 @@ export default function CinematicReplayClient() {
         reducedMotion={reducedMotion}
         onPlayPause={togglePlay}
         onScrub={scrubTo}
+        onScrubbingChange={setScrubbing}
       />
     </main>
   )
