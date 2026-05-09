0c113f456aa360032a40a804bd529b2d8fdc461a
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 14:06:29 -0500

    Restore Life Map ascent gate route authority

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,5 +1,8 @@
-import LifeMapScene from "@/components/lifemap/LifeMapScene";
+import { TierOneExperience } from '../../spatial/layout/TierOneExperience'
+import LifeMapAscentGate from '../../spatial/components/world/LifeMapAscentGate'
+
+export const canonicalLifeMapRouteAuthority = <TierOneExperience mode="life-map" />
 
 export default function LifeMapPage() {
-  return <LifeMapScene />;
+  return <LifeMapAscentGate />
 }
