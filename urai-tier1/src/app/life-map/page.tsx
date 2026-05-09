be9201512481fa930f780607f913f7f07e311530
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 14:34:49 -0500

    Fix Life Map page App Router export contract

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,8 +1,9 @@
 import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
 import LifeMapAscentGate from "@/spatial/components/world/LifeMapAscentGate";
 
-export const canonicalLifeMapRouteAuthority = <TierOneExperience mode="life-map" />;
+const canonicalLifeMapRouteAuthority = <TierOneExperience mode="life-map" />;
 
 export default function LifeMapPage() {
+  void canonicalLifeMapRouteAuthority;
   return <LifeMapAscentGate />;
 }
