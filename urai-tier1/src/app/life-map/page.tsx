0e4062728bcd3ce9e6ee19c22dd58a4030865873
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 14:31:58 -0500

    Restore canonical Life Map ascent route contract

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,14 +1,8 @@
 import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
-import { LifeMapAscentGate } from "@/spatial/lifemap/LifeMapAscentGate";
-import { LifeMapSkyPortalShell } from "@/components/lifemap/LifeMapSkyPortalShell";
+import LifeMapAscentGate from "@/spatial/components/world/LifeMapAscentGate";
 
 export const canonicalLifeMapRouteAuthority = <TierOneExperience mode="life-map" />;
-export const lifeMapSkyPortalShell = LifeMapSkyPortalShell;
 
 export default function LifeMapPage() {
-  return (
-    <LifeMapAscentGate>
-      <TierOneExperience mode="life-map" />
-    </LifeMapAscentGate>
-  );
+  return <LifeMapAscentGate />;
 }
