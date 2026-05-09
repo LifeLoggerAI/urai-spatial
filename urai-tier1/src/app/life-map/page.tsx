3d0506f07f2efee7103c7c66cd9daa49713f9cb1
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 09:23:17 -0500

    Restore canonical Life Map route shell

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,9 +1,5 @@
-import SpatialWorldCanvas from "@/spatial/components/world/SpatialWorldCanvas";
+import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
 
 export default function LifeMapPage() {
-  return (
-    <div data-testid="lifemap-starfield" data-urai-spatial-stage="life-map">
-      <SpatialWorldCanvas mode="life-map" />
-    </div>
-  );
+  return <TierOneExperience mode="life-map" />;
 }
