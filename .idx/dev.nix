5fafc456a34a207c13e822190d2b6aaf1b113b8d
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-09 07:25:27 -0500

    fix: add Playwright runtime libraries to IDX

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,6 +2,27 @@
   channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
+
+    # Playwright Chromium runtime dependencies for IDX/Firebase Studio.
+    pkgs.glib
+    pkgs.nss
+    pkgs.nspr
+    pkgs.dbus
+    pkgs.atk
+    pkgs.at-spi2-atk
+    pkgs.cups
+    pkgs.libdrm
+    pkgs.libxkbcommon
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrandr
+    pkgs.mesa
+    pkgs.alsa-lib
+    pkgs.pango
+    pkgs.cairo
+    pkgs.gtk3
   ];
   idx.extensions = [
     
