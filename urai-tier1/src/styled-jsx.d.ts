459d5134dce4a5dec24bb3cf3d618a4718b1f617
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-20 23:39:26 -0500

    Add styled-jsx JSX type shim

---
+++ urai-tier1/src/styled-jsx.d.ts
@@ -0,0 +1,8 @@
+import type * as React from "react";
+
+declare module "react" {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean;
+    global?: boolean;
+  }
+}
