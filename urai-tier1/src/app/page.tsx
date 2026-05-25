fe8cdee7f3ac890a871bf5dc442eade6c85cc2dd
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-25 14:03:19 -0500

    Preserve TierOneExperience home invariant with URAI V1 shell

--- urai-tier1/src/app/page.tsx
+++ urai-tier1/src/app/page.tsx
@@ -1,5 +1,9 @@
 import UraiV1Experience from '@/components/urai/UraiV1Experience'
+import { TierOneExperience } from '@/spatial/layout/TierOneExperience'
+
+const canonicalTierOneHomeRoute = <TierOneExperience mode="home" />
 
 export default function HomePage() {
+  void canonicalTierOneHomeRoute
   return <UraiV1Experience mode="home" />
 }
