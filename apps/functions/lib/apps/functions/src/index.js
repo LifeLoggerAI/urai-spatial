770bc993270aa5866bb5ae539cdc03d59e7e9dae
Adam Clamp <yourgithubemail@example.com>
2026-05-25 19:40:43 +0000

    Restore canonical URAI spatial runtime routes

--- apps/functions/lib/apps/functions/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleStripeWebhook = exports.evaluateSpatialTierLock = void 0;
-var tierLocks_1 = require("./tierLocks");
-Object.defineProperty(exports, "evaluateSpatialTierLock", { enumerable: true, get: function () { return tierLocks_1.evaluateSpatialTierLock; } });
-var stripeEntitlements_1 = require("./stripeEntitlements");
-Object.defineProperty(exports, "handleStripeWebhook", { enumerable: true, get: function () { return stripeEntitlements_1.handleStripeWebhook; } });
--- urai-tier1/next-env.d.ts
+++ urai-tier1/next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference path="./.next/types/routes.d.ts" />
 
-// This file is required for Next.js application typechecking.
-// Do not edit manually; keep it committed so plain TypeScript checks resolve
-// Next app-router modules and styled-jsx JSX attributes consistently.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
