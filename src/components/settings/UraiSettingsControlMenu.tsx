3158cd650bb7d6e4d51b9b202c4a4dcbb56b29da
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-03 22:00:17 -0500

    Add settings about release info

---
+++ src/components/settings/UraiSettingsControlMenu.tsx
@@ -0,0 +1,28 @@
+import {
+  URAI_APP_VERSION,
+  URAI_BUILD_LABEL,
+  URAI_RELEASE_CHANNEL,
+} from "../../lib/system/version";
+
+export function UraiSettingsControlMenu() {
+  return (
+    <section aria-labelledby="urai-settings-about-heading">
+      <h2 id="urai-settings-about-heading">About</h2>
+      <dl>
+        <div>
+          <dt>Product</dt>
+          <dd>URAI {URAI_BUILD_LABEL}</dd>
+        </div>
+        <div>
+          <dt>Version</dt>
+          <dd>{URAI_APP_VERSION}</dd>
+        </div>
+        <div>
+          <dt>Launch phase</dt>
+          <dd>{URAI_RELEASE_CHANNEL}</dd>
+        </div>
+      </dl>
+      <p>Privacy defaults remain unchanged.</p>
+    </section>
+  );
+}
