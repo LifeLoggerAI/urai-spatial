9ae9735eb6d38d0fd8ce1960192efd04bb66cb90
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-05 23:32:56 -0500

    Render Passport realm content

--- urai-tier1/src/app/passport/page.tsx
+++ urai-tier1/src/app/passport/page.tsx
@@ -1,6 +1,5 @@
-import { RealmShell } from '@/spatial/realms/RealmShell'
-import { getSceneDefinition } from '@/spatial/realms/sceneRegistry'
+import { PassportRealm } from '@/spatial/passport/PassportRealm'
 
 export default function PassportRealmPage() {
-  return <RealmShell scene={getSceneDefinition('passport')} summary="A private permission realm for understanding and controlling the data categories that power the world." />
+  return <PassportRealm />
 }
