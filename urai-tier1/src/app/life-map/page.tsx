f2705765a1ffdebe8f2ee84ed285762d221f76db
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-21 04:06:12 -0500

    Use spatial runtime for Life Map route

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,10 +1,10 @@
-import { ProductionLifeMap } from '../../life-map/ProductionLifeMap'
+import { RootModeExperience } from '../RootModeExperience'
 
 export const metadata = {
   title: 'URAI Life Map',
-  description: 'A cinematic three dimensional emotional memory constellation.',
+  description: 'Three dimensional URAI memory constellation.',
 }
 
 export default function LifeMapPage() {
-  return <ProductionLifeMap surface="canonical" />
+  return <RootModeExperience initialMode="life-map" />
 }
