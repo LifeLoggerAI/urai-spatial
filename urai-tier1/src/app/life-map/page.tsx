78cf6cd63ccc4c9784fa36fbdfc02734b0149d86
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-21 14:19:55 -0500

    Prioritize visible life map controls

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -4,8 +4,8 @@ import { RootModeExperience } from '../RootModeExperience'
 export default function LifeMapPage() {
   return (
     <>
-      <RootModeExperience initialMode="life-map" />
       <LaunchRoutePanel variant="life-map" />
+      <RootModeExperience initialMode="life-map" />
     </>
   )
 }
