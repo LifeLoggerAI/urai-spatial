b439c40a548d552fa7346c01b371a3e927602011
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-21 12:33:06 -0500

    Wire life map route panel

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -1,16 +1,11 @@
-import { LaunchSeo } from '../LaunchSeo'
+import { LaunchRoutePanel } from '../LaunchRoutePanel'
 import { RootModeExperience } from '../RootModeExperience'
-
-export const metadata = {
-  title: 'URAI Life Map',
-  description: 'The production URAI Life Map: a three-dimensional constellation with Focus and Replay handoff.',
-}
 
 export default function LifeMapPage() {
   return (
     <>
-      <LaunchSeo label="URAI Life Map renders the explorable 3D constellation." />
       <RootModeExperience initialMode="life-map" />
+      <LaunchRoutePanel variant="life-map" />
     </>
   )
 }
