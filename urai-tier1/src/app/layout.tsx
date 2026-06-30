e96ff5fad80daa3fd4dd27c71364509d290f7f37
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-30 18:25:15 -0500

    Load AAA cinematic backdrop styles

--- urai-tier1/src/app/layout.tsx
+++ urai-tier1/src/app/layout.tsx
@@ -27,6 +27,7 @@ import './home-orb-final-positioning.css'
 import './home-orb-final-motion-fix.css'
 import './home-world-screenshot-hotfix.css'
 import './launch-candidate-final-pass.css'
+import './urai-cinematic-backdrop.css'
 import UraiCinematicBackdrop from './UraiCinematicBackdrop'
 
 export const metadata: Metadata = {
@@ -52,7 +53,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <UraiCinematicBackdrop />
         {children}
-</body>
+      </body>
     </html>
   )
 }
