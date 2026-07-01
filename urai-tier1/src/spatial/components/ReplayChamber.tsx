6e9597fdb13df24abec55e29fb87660beeee5d66
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-30 22:43:34 -0500

    Default optional star intensity in Replay chamber

--- urai-tier1/src/spatial/components/ReplayChamber.tsx
+++ urai-tier1/src/spatial/components/ReplayChamber.tsx
@@ -3,13 +3,11 @@
 import React from 'react'
 import type { LifeMapStar } from '@/lib/uraiCanon/lifemapStar'
 
-export default function ReplayChamber(props: {
-  star: LifeMapStar | null
-  visible: boolean
-}) {
+export default function ReplayChamber(props: { star: LifeMapStar | null; visible: boolean }) {
   if (!props.visible) return null
 
   const p = props.star?.position ?? [0, 0, 0]
+  const intensity = props.star?.intensity ?? 0.5
 
   return (
     <>
@@ -17,25 +15,14 @@ export default function ReplayChamber(props: {
       <pointLight position={[p[0], p[1], p[2] + 1.4]} intensity={1.4} color="#f1ccff" />
       <mesh position={[p[0], p[1], p[2] - 1.2]}>
         <sphereGeometry args={[3.2, 48, 48]} />
-        <meshStandardMaterial
-          color="#1a1022"
-          emissive="#14081b"
-          emissiveIntensity={0.35}
-          transparent
-          opacity={0.92}
-          side={2}
-        />
+        <meshStandardMaterial color="#1a1022" emissive="#14081b" emissiveIntensity={0.35} transparent opacity={0.92} side={2} />
       </mesh>
-      {props.star && (
+      {props.star ? (
         <mesh position={p} scale={[1.6, 1.6, 1.6]}>
-          <sphereGeometry args={[0.18 + props.star.intensity * 0.18, 28, 28]} />
-          <meshStandardMaterial
-            color="#fff0ff"
-            emissive="#d08dff"
-            emissiveIntensity={2.1}
-          />
+          <sphereGeometry args={[0.18 + intensity * 0.18, 28, 28]} />
+          <meshStandardMaterial color="#fff0ff" emissive="#d08dff" emissiveIntensity={2.1} />
         </mesh>
-      )}
+      ) : null}
     </>
   )
 }
