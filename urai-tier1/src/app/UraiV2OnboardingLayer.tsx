56155713671e3d22306c253d820916a12f632a3f
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-03 04:24:58 -0500

    Load the live V2 interaction styles

--- urai-tier1/src/app/UraiV2OnboardingLayer.tsx
+++ urai-tier1/src/app/UraiV2OnboardingLayer.tsx
@@ -12,9 +12,11 @@ import {
 import "./v2-ground-states.css";
 import "./v2-ground-council.css";
 import "./v2-ground-objects.css";
+import "./v2-ground-interaction.css";
 import "./v2-memory-states.css";
 import "./v2-realm-states.css";
 import "./v2-accessibility-states.css";
+import "./v2-state-controller.css";
 import "./v2-onboarding.css";
 
 const cards = {
