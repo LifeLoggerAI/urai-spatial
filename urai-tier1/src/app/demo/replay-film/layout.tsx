4cc8bb83a5d52203950b6cc0362ffa889fb138cc
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-02 23:39:19 -0500

    Add Replay proof visual shell

---
+++ urai-tier1/src/app/demo/replay-film/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from 'react'
+import './aaa-mobile.css'
+
+export default function ReplayFilmLayout({ children }: { children: ReactNode }) {
+  return <div className="urai-replay-film-route">{children}</div>
+}
