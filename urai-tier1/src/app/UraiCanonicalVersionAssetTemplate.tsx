567973ce6d86c079125ec1005b6b32cafb61ce8c
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-04 01:01:28 -0500

    Compose canonical version asset runtime

---
+++ urai-tier1/src/app/UraiCanonicalVersionAssetTemplate.tsx
@@ -0,0 +1,7 @@
+import UraiCanonicalVersionAssetGates from './UraiCanonicalVersionAssetGates'
+import './v3-relationship-asset-wiring.css'
+import './v4-xr-asset-wiring.css'
+
+export default function UraiCanonicalVersionAssetTemplate() {
+  return <UraiCanonicalVersionAssetGates />
+}
