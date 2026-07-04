f715196125d32b17d2d06a0792f6a1589cba4a95
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-04 01:12:52 -0500

    Use compact canonical asset gates

--- urai-tier1/src/app/UraiCanonicalVersionAssetTemplate.tsx
+++ urai-tier1/src/app/UraiCanonicalVersionAssetTemplate.tsx
@@ -1,7 +1,7 @@
-import UraiCanonicalVersionAssetGates from './UraiCanonicalVersionAssetGates'
+import CanonicalAssetGates from './CanonicalAssetGates'
 import './v3-relationship-asset-wiring.css'
 import './v4-xr-asset-wiring.css'
 
 export default function UraiCanonicalVersionAssetTemplate() {
-  return <UraiCanonicalVersionAssetGates />
+  return <CanonicalAssetGates />
 }
