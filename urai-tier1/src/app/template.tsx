a9f0cb2e38b5325b515f52d790de80e472cd6214
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-04 03:28:57 -0500

    Load the WebGL fallback restoration after spatial Home styles

--- urai-tier1/src/app/template.tsx
+++ urai-tier1/src/app/template.tsx
@@ -1,5 +1,6 @@
 import HomeSpatialRuntimeLayer from './HomeSpatialRuntimeLayer'
 import './home-spatial-runtime-layer.css'
+import './home-spatial-runtime-fallback.css'
 
 export default function AppTemplate({ children }: { children: React.ReactNode }) {
   return (
