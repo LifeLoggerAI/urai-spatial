18e19562443138cd6a31ac98f61d20de83f4679e
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-06 16:04:06 -0500

    Load V5 canonical asset wiring

--- urai-tier1/src/app/UraiCanonicalVersionAssetTemplate.tsx
+++ urai-tier1/src/app/UraiCanonicalVersionAssetTemplate.tsx
@@ -1,6 +1,7 @@
 import CanonicalAssetGates from './CanonicalAssetGates'
 import './v3-relationship-asset-wiring.css'
 import './v4-xr-asset-wiring.css'
+import './v5-becoming-asset-wiring.css'
 
 export default function UraiCanonicalVersionAssetTemplate() {
   return <CanonicalAssetGates />
