9427facaa84f54bf4bea9b790f6b86c26c7c67a2
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-06 02:29:07 -0500

    feat: build XR sanctuary environment

---
+++ urai-tier1/src/app/spatial/ar-vr/xrEntryPremiumBuild.ts
@@ -0,0 +1,23 @@
+import * as THREE from 'three'
+import { createAtmosphere, createFloor, createSky } from './xrEntrySkyAndParticles'
+import { addArrivalDais, addLighting, addSanctuaryArchitecture } from './xrEntrySanctuaryGeometry'
+import { createLivingOrb } from './xrEntryLivingOrb'
+import { isolateAnimatedMaterials, normalizeTransmission, shareStaticMaterial } from './xrEntryRingMaterialIsolation'
+import type { PremiumEnvironment } from './xrEntryPremiumTypes'
+
+export function buildPremiumEnvironment(scene: THREE.Scene, mobile: boolean): PremiumEnvironment {
+  scene.background = new THREE.Color(0x071325)
+  scene.fog = new THREE.Fog(0x10243a, 20, 92)
+  const sky = createSky(mobile)
+  const floor = createFloor(mobile)
+  const atmosphere = createAtmosphere(mobile)
+  scene.add(sky, floor, atmosphere.stars, atmosphere.motes)
+  addLighting(scene, mobile)
+  const glowRings = addArrivalDais(scene, mobile)
+  isolateAnimatedMaterials(glowRings)
+  const accentLights = addSanctuaryArchitecture(scene, mobile)
+  const orbParts = createLivingOrb(scene, mobile)
+  shareStaticMaterial(orbParts.orbRings)
+  normalizeTransmission(scene, mobile)
+  return { floor, sky, glowRings, accentLights, ...atmosphere, ...orbParts }
+}
