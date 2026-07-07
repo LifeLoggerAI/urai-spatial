1d17cf13166b4e5c6a0e70142247bda2bcfa4560
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-07 08:38:44 -0500

    Move universe stream route to static export compatible path

---
+++ urai-tier1/src/app/api/universe-stream/route.ts
@@ -0,0 +1,54 @@
+import { runProductionOrchestrator } from "../../../../automation/productionOrchestrator";
+
+export const dynamic = "force-static";
+
+function createSnapshot(tick = 0) {
+  return {
+    worlds: [],
+    memoryGraph: { nodes: [] },
+    interactions: { messages: [] },
+    emergence: {
+      globalCoherence: 0.5,
+      entropy: 0.2,
+    },
+    tick,
+  };
+}
+
+export async function GET() {
+  if (process.env.URAI_FIREBASE_STATIC_EXPORT === "true") {
+    return Response.json({
+      result: runProductionOrchestrator(createSnapshot()),
+      mode: "static-preview",
+    });
+  }
+
+  const encoder = new TextEncoder();
+  let tick = 0;
+
+  const stream = new ReadableStream({
+    start(controller) {
+      const interval = setInterval(() => {
+        tick++;
+        controller.enqueue(
+          encoder.encode(
+            `data: ${JSON.stringify(runProductionOrchestrator(createSnapshot(tick)))}\n\n`,
+          ),
+        );
+        if (tick > 50) {
+          clearInterval(interval);
+          controller.close();
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    },
+  });
+
+  return new Response(stream, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+    },
+  });
+}
