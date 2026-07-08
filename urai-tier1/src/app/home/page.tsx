745eefcb0ff92115c8822f2ed793a4a98e210ffc
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-07 21:34:55 -0500

    Surface asset factory slots on Home route

--- urai-tier1/src/app/home/page.tsx
+++ urai-tier1/src/app/home/page.tsx
@@ -1,5 +1,11 @@
+import { AssetFactoryRoutePanel } from '@/components/urai/assets/AssetFactoryRoutePanel'
 import SpatialRealPlaceWorld from '@/app/SpatialRealPlaceWorld'
 
 export default function HomeRoutePage() {
-  return <SpatialRealPlaceWorld mode="home" />
+  return (
+    <>
+      <SpatialRealPlaceWorld mode="home" />
+      <AssetFactoryRoutePanel route="/home" title="Home Asset Factory" />
+    </>
+  )
 }
