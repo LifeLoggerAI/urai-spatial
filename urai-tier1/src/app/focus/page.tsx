3ffcbe4f0b6c269ef8cd9e86b19222daa2d4c256
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-07 21:38:09 -0500

    Surface asset factory slots on Focus route

--- urai-tier1/src/app/focus/page.tsx
+++ urai-tier1/src/app/focus/page.tsx
@@ -1,5 +1,11 @@
+import { AssetFactoryRoutePanel } from '@/components/urai/assets/AssetFactoryRoutePanel'
 import M from '../MemoryStarJourneyWorld'
 
 export default function FocusRoutePage() {
-  return <M mode="focus" />
+  return (
+    <>
+      <M mode="focus" />
+      <AssetFactoryRoutePanel route="/focus" title="Focus Asset Factory" />
+    </>
+  )
 }
