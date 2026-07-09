ea830b2d102f6e0fbe99e2163b6d75d63638e681
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-09 00:39:09 -0500

    Keep Spatial Ground asset panel copy claim-safe

--- urai-tier1/src/app/ground/page.tsx
+++ urai-tier1/src/app/ground/page.tsx
@@ -10,7 +10,7 @@ export default function GroundPage() {
   return (
     <>
       <GroundSpatialWorldClean />
-      <AssetFactoryRoutePanel route="/ground" title="Ground Asset Factory" />
+      <AssetFactoryRoutePanel route="/ground" title="Ground Launch Asset Pipeline" />
     </>
   )
 }
