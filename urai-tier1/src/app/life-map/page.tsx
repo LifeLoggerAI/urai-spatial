618d62f38c8de06c30ec392a19e9b1bfdef9685a
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-09 00:36:40 -0500

    Keep Spatial Life Map asset panel copy claim-safe

--- urai-tier1/src/app/life-map/page.tsx
+++ urai-tier1/src/app/life-map/page.tsx
@@ -5,7 +5,7 @@ export default function LifeMapPage() {
   return (
     <>
       <SpatialRealPlaceWorld mode="life-map" />
-      <AssetFactoryRoutePanel route="/life-map" title="Life Map Asset Factory" />
+      <AssetFactoryRoutePanel route="/life-map" title="Life Map Launch Asset Pipeline" />
     </>
   )
 }
