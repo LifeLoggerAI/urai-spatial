6a3c55df21e3d55047d70a93b5367eafef5a2a9b
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-15 18:21:13 -0500

    fix: import Ground star field owner

--- urai-tier1/src/app/GroundSpatialWorldClean.tsx
+++ urai-tier1/src/app/GroundSpatialWorldClean.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Html, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Html, OrbitControls, PerspectiveCamera, Stars } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Bloom, EffectComposer, Vignette } from '@react-three/postprocessing'
 import Link from 'next/link'
