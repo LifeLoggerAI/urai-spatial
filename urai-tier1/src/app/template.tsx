1d0b05d88a3aed3b85653d72efddb17193541d7d
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-15 17:15:39 -0500

    fix(spatial): load structural proof corrections

--- urai-tier1/src/app/template.tsx
+++ urai-tier1/src/app/template.tsx
@@ -1,5 +1,6 @@
 import HomeSpatialRuntimeLayer from './HomeSpatialRuntimeLayer'
 import './spatial-runtime-restoration.css'
+import './continuous-spatial-proof-defects.css'
 
 export default function AppTemplate({ children }: { children: React.ReactNode }) {
   return (
