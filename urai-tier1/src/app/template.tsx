1be576f8b159edd0e226e49aac4d16426b632a32
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-15 18:02:49 -0500

    chore(home): load WebGL veil fix

--- urai-tier1/src/app/template.tsx
+++ urai-tier1/src/app/template.tsx
@@ -1,6 +1,7 @@
 import HomeSpatialRuntimeLayer from './HomeSpatialRuntimeLayer'
 import './spatial-runtime-restoration.css'
 import './continuous-spatial-proof-defects.css'
+import './home-webgl-legacy-owner-fix.css'
 
 export default function AppTemplate({ children }: { children: React.ReactNode }) {
   return (
