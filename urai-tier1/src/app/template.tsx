3e1f073decb6459eb78d1c8319556ebfe59dd0fa
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-15 17:16:55 -0500

    Mount exact Life Map deep-link restoration

--- urai-tier1/src/app/template.tsx
+++ urai-tier1/src/app/template.tsx
@@ -1,10 +1,13 @@
 import HomeSpatialRuntimeLayer from './HomeSpatialRuntimeLayer'
+import LifeMapDeepLinkRestoration from './LifeMapDeepLinkRestoration'
 import './spatial-runtime-restoration.css'
+import './spatial-proof-repair.css'
 
 export default function AppTemplate({ children }: { children: React.ReactNode }) {
   return (
     <>
       <HomeSpatialRuntimeLayer />
+      <LifeMapDeepLinkRestoration />
       {children}
     </>
   )
