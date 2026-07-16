f0ffcef938b730cb375f5f8fee0ba0d8f320f1e2
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-15 19:23:52 -0500

    fix: load final mobile composition override

--- urai-tier1/src/app/template.tsx
+++ urai-tier1/src/app/template.tsx
@@ -1,6 +1,7 @@
 import HomeSpatialRuntimeLayer from './HomeSpatialRuntimeLayer'
 import './spatial-runtime-restoration.css'
 import './continuous-spatial-proof-defects.css'
+import './premium-mobile-composition.css'
 
 export default function AppTemplate({ children }: { children: React.ReactNode }) {
   return (
