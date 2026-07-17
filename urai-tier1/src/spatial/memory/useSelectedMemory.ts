c54f1f2ecbee3ba978c5a932d401f7105ca28e6f
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-17 08:27:16 -0500

    feat(memory): hydrate selected memory without silent seed fallback

---
+++ urai-tier1/src/spatial/memory/useSelectedMemory.ts
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { app, firebasePublicEnvReady, getFirebaseDb } from '@/lib/firebase/client'
+import {
+  buildExplicitDemoMemory,
+  isExplicitDemoRequest,
+  parseSelectedMemory,
+  sanitizeMemoryId,
+  type SelectedMemoryResult,
+} from './selectedMemoryContract'
+
+const LOADING:
