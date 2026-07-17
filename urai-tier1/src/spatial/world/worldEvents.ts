41df63bfdee7b51347206615d27eceae5ae55edc
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-16 20:33:30 -0500

    fix(spatial): restore complete world event contract

--- urai-tier1/src/spatial/world/worldEvents.ts
+++ urai-tier1/src/spatial/world/worldEvents.ts
@@ -5,4 +5,24 @@ export const URAI_WORLD_RETURN_EVENT = 'urai:world-return'
 export const URAI_WORLD_ORB_OPEN_EVENT = 'urai:world-orb-open'
 
 export function requestUraiWorldTravel(request: UraiWorldTravelRequest) {
-  if (typeof window === 'undefined
+  if (typeof window === 'undefined') return
+  window.dispatchEvent(new CustomEvent<UraiWorldTravelRequest>(URAI_WORLD_TRAVEL_EVENT, { detail: request }))
+}
+
+export function requestUraiWorldReturn() {
+  if (typeof window === 'undefined') return
+  window.dispatchEvent(new Event(URAI_WORLD_RETURN_EVENT))
+}
+
+export function requestUraiWorldOrbOpen() {
+  if (typeof window === 'undefined') return
+  window.dispatchEvent(new Event(URAI_WORLD_ORB_OPEN_EVENT))
+}
+
+declare global {
+  interface WindowEventMap {
+    [URAI_WORLD_TRAVEL_EVENT]: CustomEvent<UraiWorldTravelRequest>
+    [URAI_WORLD_RETURN_EVENT]: Event
+    [URAI_WORLD_ORB_OPEN_EVENT]: Event
+  }
+}
