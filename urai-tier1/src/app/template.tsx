638cbe6bc5ab3c973ab2481e1663e6fffa478ff9
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-17 07:51:52 -0500

    Load final Home sanctuary visual authority

--- urai-tier1/src/app/template.tsx
+++ urai-tier1/src/app/template.tsx
@@ -2,6 +2,7 @@ import HomeSpatialRuntimeLayer from './HomeSpatialRuntimeLayer'
 import './spatial-runtime-restoration.css'
 import './continuous-spatial-proof-defects.css'
 import './premium-spatial-atmosphere.css'
+import './home-sanctuary-final.css'
 
 export default function AppTemplate({ children }: { children: React.ReactNode }) {
   return (
