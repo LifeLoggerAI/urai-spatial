31ccae5b8b1d3de5f1c6d1f6df9c45fb14f32843
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-21 00:38:47 -0500

    fix(life-map): restore valid canonical route boundary

--- urai-tier1/src/components/lifemap/LifeMapRouteBoundary.tsx
+++ urai-tier1/src/components/lifemap/LifeMapRouteBoundary.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { useEffect } from 'react'
 import AdaptiveLifeMapScene from './AdaptiveLifeMapScene'
 
-const overviewActionLabels = new Set(['Overview', 'Open semantic overview'])
-
 export default function LifeMapRouteBoundary() {
-  useEffect(() => {
-    const primeOverviewHistory = (event: MouseEvent) => {
+  return <AdaptiveLifeMapScene />
+}
