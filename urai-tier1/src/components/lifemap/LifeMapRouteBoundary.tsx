a82052d63d9bb77a99824eca07c4e1506d172f20
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-23 22:08:39 -0500

    fix(life-map): initialize Three semantic data path support

--- urai-tier1/src/components/lifemap/LifeMapRouteBoundary.tsx
+++ urai-tier1/src/components/lifemap/LifeMapRouteBoundary.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import './installR3FDataProps'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import ComposedLifeMapScene from './ComposedLifeMapScene'
