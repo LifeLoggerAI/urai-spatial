7e1a0cbbac6fdac635fcf23caac7c70d7fb810a5
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-25 19:06:51 -0500

    fix(world): defer hard travel fallback past deep transition

--- urai-tier1/src/spatial/world/worldEvents.ts
+++ urai-tier1/src/spatial/world/worldEvents.ts
@@ -5,7 +5,7 @@ export const URAI_WORLD_RETURN_EVENT = 'urai:world-return'
 export const URAI_WORLD_ORB_OPEN_EVENT = 'urai:world-orb-open'
 
 const WORLD_TRAVEL_DEBOUNCE_MS = 1500
-const WORLD_TRAVEL_FALLBACK_MS = 1800
+const WORLD_TRAVEL_FALLBACK_MS = 2400
 let lastTravelFingerprint = ''
 let lastTravelAt = 0
 
