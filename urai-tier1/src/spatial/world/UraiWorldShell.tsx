34afd67c5845f5a07f0f56b1379fdb399246f22c
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-28 18:09:24 -0500

    Wire transactional Life Map route ownership repair

--- urai-tier1/src/spatial/world/UraiWorldShell.tsx
+++ urai-tier1/src/spatial/world/UraiWorldShell.tsx
@@ -3,6 +3,7 @@
 import type { ReactNode } from 'react'
 import { GroundGateway } from './GroundGateway'
 import { LifeMapIndependentInputBoundary } from './LifeMapIndependentInputBoundary'
+import { LifeMapRouteTransactionBridge } from './LifeMapRouteTransactionBridge'
 import { LifeMapSelectedActionRuntimeInvariant } from './LifeMapSelectedActionRuntimeInvariant'
 import { PersistentRealmAtmosphere } from './PersistentRealmAtmosphere'
 import { PersistentWorldCompanion } from './PersistentWorldCompanion'
@@ -44,6 +45,7 @@ export function UraiWorldShell({ children }: { children: ReactNode }) {
       <PersistentRealmAtmosphere />
       {children}
       <GroundGateway />
+      {world.destination === 'life-map' ? <LifeMapRouteTransactionBridge /> : null}
       {world.destination === 'life-map' ? <LifeMapIndependentInputBoundary /> : null}
       {showWorldCompanion ? <PersistentWorldCompanion /> : null}
       <WorldTransitionController />
