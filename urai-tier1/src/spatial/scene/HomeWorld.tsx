e0539e501b7feb49e48ef8f3d45c4f6c7bf5dd99
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-08-05 13:39:30 -0500

    Remove placeholder Home geometry

--- urai-tier1/src/spatial/scene/HomeWorld.tsx
+++ urai-tier1/src/spatial/scene/HomeWorld.tsx
@@ -94,13 +94,13 @@ export default function HomeWorld() {
       />
 
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-0.52, 0.012, -0.05]} receiveShadow>
-        <circleGeometry args={[1.1, 36]} />
-        <shadowMaterial opacity={0.5} />
+        <circleGeometry args={[1.1, 48]} />
+        <shadowMaterial opacity={0.42} />
       </mesh>
 
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-0.48, 0.014, -0.08]}>
-        <circleGeometry args={[1.4, 40]} />
-        <meshBasicMaterial color="#67c4ff" transparent opacity={0.08} depthWrite={false} />
+        <ringGeometry args={[0.78, 1.48, 72]} />
+        <meshBasicMaterial color="#67c4ff" transparent opacity={0.12} depthWrite={false} />
       </mesh>
 
       <Orb
@@ -133,21 +133,6 @@ export default function HomeWorld() {
       </Html>
 
       <PresenceRig />
-
-      <mesh position={[-4.2, 1.3, -3.2]} castShadow receiveShadow>
-        <boxGeometry args={[0.36, 2.6, 0.36]} />
-        <meshStandardMaterial color="#04060d" transparent opacity={0.2} roughness={1} metalness={0} />
-      </mesh>
-
-      <mesh position={[-2.8, 1.6, -5.4]} castShadow receiveShadow>
-        <boxGeometry args={[0.44, 3.2, 0.44]} />
-        <meshStandardMaterial color="#04060d" transparent opacity={0.16} roughness={1} metalness={0} />
-      </mesh>
-
-      <mesh position={[3.4, 1.4, -4.8]} castShadow receiveShadow>
-        <boxGeometry args={[0.4, 2.8, 0.4]} />
-        <meshStandardMaterial color="#04060d" transparent opacity={0.18} roughness={1} metalness={0} />
-      </mesh>
     </group>
   );
 }
