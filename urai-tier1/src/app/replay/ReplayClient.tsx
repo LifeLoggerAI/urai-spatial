7823f5de31e6c8cc7bbf88954229cb0d44cce639
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-08-07 19:55:30 -0500

    Remove dead legacy replay client

--- urai-tier1/src/app/replay/ReplayClient.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TierOneExperience } from "@/spatial/layout/TierOneExperience";
-
-export default function ReplayClient() {
-  return <TierOneExperience mode="replay" />;
-}
