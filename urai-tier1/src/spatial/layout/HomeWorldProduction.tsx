52c2d485fc26db63619dc628b02efc29c453de59
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-08-09 13:58:59 -0500

    Restore canonical Home Final owner export

--- urai-tier1/src/spatial/layout/HomeWorldProduction.tsx
+++ urai-tier1/src/spatial/layout/HomeWorldProduction.tsx
@@ -1,3 +1,3 @@
 "use client";
 
-export { HomeWorldProductionCinematic as HomeWorldProduction } from "./HomeWorldProductionCinematic";
+export { HomeWorldProductionFinal as HomeWorldProduction } from "./HomeWorldProductionFinal";
