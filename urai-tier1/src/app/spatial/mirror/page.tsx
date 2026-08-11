fe8a1360558b95bd6a1f93a8a1b21ae6cf006451
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-08-11 06:34:10 -0500

    add physical Mirror world route

---
+++ urai-tier1/src/app/spatial/mirror/page.tsx
@@ -0,0 +1,10 @@
+import MirrorRealWorldRealm from '@/spatial/realms/MirrorRealWorldRealm'
+
+export const metadata = {
+  title: 'URAI Mirror Chamber',
+  description: 'Enter the physical Mirror chamber before opening the governed reflection layer.',
+}
+
+export default function SpatialMirrorPage() {
+  return <MirrorRealWorldRealm />
+}
