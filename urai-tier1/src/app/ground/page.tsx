de37f63bbc38d0a153b232c7898eacd52c43fa67
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-09-16 12:36:09 -0500

    fix(ground): mount semantic return bridge

--- urai-tier1/src/app/ground/page.tsx
+++ urai-tier1/src/app/ground/page.tsx
@@ -1,6 +1,7 @@
 import GroundSpatialWorldClean from '@/app/GroundSpatialWorldClean'
 import GroundPersonalizationBoundary from './GroundPersonalizationBoundary'
 import GroundGeographicLivedWorldBridge from './GroundGeographicLivedWorldBridge'
+import GroundSemanticReturnBridge from './GroundSemanticReturnBridge'
 import './ground-production-polish.css'
 import { getSceneDefinition } from '@/spatial/realms/sceneRegistry'
 
@@ -20,6 +21,7 @@ export default function GroundPage() {
     >
       <GroundPersonalizationBoundary />
       <GroundGeographicLivedWorldBridge />
+      <GroundSemanticReturnBridge />
       <GroundSpatialWorldClean />
     </main>
   )
